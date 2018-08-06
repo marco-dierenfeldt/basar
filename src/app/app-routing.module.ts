aa43962b4deb2a6bd41ac2edcc649a8f5cb9559f
Marco Dierenfeldt <marco.dierenfeldt@pentasys.de>
2018-08-06 07:10:13 +0200

    add routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/compiler/src/core';
+import {RouterModule, Routes} from '@angular/router';
+
+@NgModule({
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule() {
+
+}
